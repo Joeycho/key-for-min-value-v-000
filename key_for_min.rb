94980d6df56b27848a1d70ac7380a9d156371ff2
Joey Cho <cjy12182000@naver.com>
2017-10-04 22:13:39 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,11 +8,13 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < temp
       temp = value
+    end
   end
 
   name_hash.collect do |key, value|
     if value = temp
       key
+    end
   end
 
 end
